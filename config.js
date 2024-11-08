014d5e5cdc819d95a85c0267385525fd3eec0e10
“Dwight <dwighthowichill@icloud.com>
2024-11-08 10:45:16 -0500

    updated restify

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 4000,
-    URL : process.env.BASE_URL || 'http://localhost:4000',
+    URL: process.env.BASE_URL || 'http://localhost:4000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://carter91jr:howichill18@customerapi.wunlb.mongodb.net/?retryWrites=true&w=majority&appName=CustomerApi',
-    JWT_SECRET:process.env.JWT_SECRET || 'secret1'
-}
+    JWT_SECRET: process.env.JWT_SECRET || 'secret1'
+  }
